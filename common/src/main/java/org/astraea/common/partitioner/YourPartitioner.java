306cb7ce001014678ed82f99619f328843d93a24
Leon P Wang <leon_p_wang@trendmicro.com>
2024-11-10 17:19:23 +0800

    impl custom partitioner algo

--- common/src/main/java/org/astraea/common/partitioner/YourPartitioner.java
+++ common/src/main/java/org/astraea/common/partitioner/YourPartitioner.java
@@ -16,15 +16,40 @@
  */
 package org.astraea.common.partitioner;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+
 import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 
 public class YourPartitioner implements Partitioner {
 
+  // not use atomic integer here to reduce implementation complexity
+  private Map<Integer, Integer> brokerCounts;
+  private Map<Integer, HashSet<Integer>> brokerPartitionMap;
+  private Map<Integer, Integer> partitionCounts;
+
   // get your magic configs
   @Override
-  public void configure(Map<String, ?> configs) {}
+  public void configure(Map<String, ?> configs) {
+    brokerCounts = new HashMap<>();
+    brokerPartitionMap = new HashMap<>();
+    partitionCounts = new HashMap<>();
+  }
+
+  private int getKeyWithMinCount(Map<Integer, Integer> map) {
+    if (map.isEmpty()) { return -1; }
+    Integer minKey = null;
+    Integer minValue = Integer.MAX_VALUE;
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      if (entry.getValue() < minValue) {
+        minValue = entry.getValue();
+        minKey = entry.getKey();
+      }
+    }
+    return minKey != null ? minKey : -1; // Return -1 if no key was found
+  }
 
   // write your magic code
   @Override
@@ -33,10 +58,34 @@ public class YourPartitioner implements Partitioner {
     var partitions = cluster.availablePartitionsForTopic(topic);
     // no available partition so we return -1
     if (partitions.isEmpty()) return -1;
-    System.out.println("debug: cluster information");
-    System.out.println(cluster.toString());
-    var numOfPartitions = cluster.partitionCountForTopic(topic);
-    return (key.hashCode() % numOfPartitions + numOfPartitions) % numOfPartitions;
+
+    // init maps
+    for (var partition : partitions) {
+      var brokerId = partition.leader().id();
+      var partitionId = partition.partition();
+      brokerCounts.putIfAbsent(brokerId, 0);
+      partitionCounts.putIfAbsent(partitionId, 0);
+
+      HashSet<Integer> instances = brokerPartitionMap.getOrDefault(brokerId, new HashSet<>());
+      instances.add(partition.partition());
+      brokerPartitionMap.put(brokerId, instances);
+    }
+
+    var minCountBrokerId = getKeyWithMinCount(brokerCounts);
+    var candidatePartitions = brokerPartitionMap.get(minCountBrokerId);
+
+    Integer partitionId = -1;
+    Integer minCount = Integer.MAX_VALUE;
+    for (var candidate : candidatePartitions) {
+      var partitionCount = partitionCounts.get(candidate);
+      if (partitionCount < minCount) {
+        partitionId = candidate;
+        minCount = partitionCount;
+      }
+    }
+    brokerCounts.put(minCountBrokerId, brokerCounts.get(minCountBrokerId) + 1);
+    partitionCounts.put(partitionId, partitionCounts.get(partitionId) + 1);
+    return partitionId;
   }
 
   @Override
