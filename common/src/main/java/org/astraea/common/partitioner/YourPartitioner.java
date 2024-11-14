732f044fc8c9fa34b2e4f263e3c60561346f1f9f
Leon P Wang <leon_p_wang@trendmicro.com>
2024-11-14 09:44:50 +0800

    Revert "add list to monitor the final results"
    This reverts commit 39f6ab49002359c85c5f6aedf994e5d64ee4981a.

--- common/src/main/java/org/astraea/common/partitioner/YourPartitioner.java
+++ common/src/main/java/org/astraea/common/partitioner/YourPartitioner.java
@@ -16,7 +16,6 @@
  */
 package org.astraea.common.partitioner;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -35,17 +34,11 @@ public class YourPartitioner implements Partitioner {
   private Map<Integer, Set<Integer>> brokerPartitionMap;
   private Map<Integer, AtomicInteger> partitionCounts;
 
-  private List<Integer> brokerList;
-  private List<Integer> partitionList;
-
   @Override
   public void configure(Map<String, ?> configs) {
     brokerCounts = new ConcurrentHashMap<>();
     brokerPartitionMap = new ConcurrentHashMap<>();
     partitionCounts = new ConcurrentHashMap<>();
-
-    brokerList = new ArrayList<>();
-    partitionList = new ArrayList<>();
   }
 
   private int getKeyWithMinCount(Map<Integer, AtomicInteger> map) {
@@ -60,7 +53,8 @@ public class YourPartitioner implements Partitioner {
   public int partition(
           String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
 
-    List<PartitionInfo> partitions = cluster.availablePartitionsForTopic(topic);
+//    List<PartitionInfo> partitions = cluster.availablePartitionsForTopic(topic);
+    List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
     if (partitions.isEmpty()) {
       return -1;
     }
@@ -92,23 +86,11 @@ public class YourPartitioner implements Partitioner {
     if (partitionId != -1) {
       brokerCounts.get(minCountBrokerId).incrementAndGet();
       partitionCounts.get(partitionId).incrementAndGet();
-
-      brokerList.add(minCountBrokerId);
-      partitionList.add(partitionId);
     }
 
     return partitionId;
   }
 
   @Override
-  public void close() {
-    StringBuilder brokerListBuilder = new StringBuilder("brokerList: ");
-    brokerList.forEach(num -> brokerListBuilder.append(num).append(" "));
-
-    StringBuilder partitionListBuilder = new StringBuilder("partitionList: ");
-    partitionList.forEach(num -> partitionListBuilder.append(num).append(" "));
-
-    System.out.println(brokerListBuilder.toString().trim());
-    System.out.println(partitionListBuilder.toString().trim());
-  }
+  public void close() {}
 }
